188f97aba86c22e3ead542d17b76d1d87dad3d22
Faheem-saif <faheem000saif@gmail.com>
2023-10-29 23:43:08 +0500

    f string methods

--- 01_class/pythonclass.py
+++ 01_class/pythonclass.py
@@ -1,3 +1,29 @@
 print("My Name is Faheem")
 name :str="faheem Saif"
 print(name)
+
+fname:str = "Faheem"
+lname:str = "Saif"
+age:int = 23
+education:str  = "BS Computer science"
+card:any = f"""PIAIC Student card
+Student Name: {fname+" "+ lname}
+ Age: {age} 
+ Education: {education}
+" Print the card"  """
+print(card)
+# string boundries
+# "'", '"' use
+name:str = "I'm Faheem"
+print(name)
+# backslash \ IS USED  for the conversion of any special character to normal character
+name2:str = 'I\'m Faheem'
+print(name2)
+
+
+import re 
+name: str = "           Faheem           saif    " 
+display(name)
+
+name1: str = re.sub('{2,100}',' ',name )
+display(name1)
